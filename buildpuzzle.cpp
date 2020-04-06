bcb065320e12cfa285ae95db7aad4ac7f90a752a
PaoVal <paolaval@ualberta.ca>
2020-04-05 22:44:06 -0600

    fixed 1.5

--- buildpuzzle.cpp
+++ buildpuzzle.cpp
@@ -56,36 +56,53 @@ void Puzzle::check_overlap(uint16_t total, uint16_t perpTotal, vector<vector<uin
 // private helper method
 // if you find nothing, send n_cols +1
 // i just realized this could be better but i'm tired. maybe i'll fix later :(((
-int16_t Puzzle::find_white_or_wall(int16_t bottomLim, int16_t topLim, int8_t incr_or_decr) {
+int16_t Puzzle::find_white_or_wall(int16_t bottomLim, int16_t topLim, int8_t incr_or_decr, uint16_t perpTotal, 
+	uint16_t line, bool isCol) {
 
 	// going up
 	if (incr_or_decr == 1) {
 		// try to find a white
-		while (bottomLim <=topLim) {
+		while (bottomLim <=topLim && bottomLim < perpTotal) {
+			uint16_t index = n_cols*line+bottomLim;
+			if (isCol) {
+				index = n_cols*bottomLim+line;
+			}
+			//cout << "index: " << index << endl;
 			// if you find a white, return the cell
-			if (cells[bottomLim] == 1) {
+			if (cells[index] == 1) {
 				return bottomLim;
 			}
-			bottomLim = bottomLim + incr_or_decr;
+			bottomLim++;
 		}
 		// still nothing? check if there's a wall
-		if (topLim >=n_cols) {
-			return n_cols;
+		if (topLim >=perpTotal) {
+			return perpTotal;
 		}
 		// found nothing
 		else{
-			return n_cols +1;
+			//cout << "NOPE ";
+			return -2;
 		}
 	}
 	// going down
 	else {
+		// cout << "bottom? " << bottomLim << " top? " <<topLim <<endl;
+		// cout <<endl; 
 		// try to find a white
-		while (bottomLim >=topLim) {
+		while (bottomLim <=topLim && topLim >= 0) {
+			uint16_t index = n_cols*line+topLim;
+			if (isCol) {
+				index = n_cols*topLim+line;
+			}
+
+			//cout << "index: " << index << endl;
 			// if you find a white, return the cell
-			if (cells[topLim] == 1) {
+			if (cells[index] == 1) {
 				return topLim;
 			}
-			topLim = topLim + incr_or_decr;
+			//cout << "top: " << topLim << ", ";
+			topLim--;
+
 		}
 		// still nothing? check if there's a wall
 		if (bottomLim <= -1) {
@@ -93,20 +110,28 @@ int16_t Puzzle::find_white_or_wall(int16_t bottomLim, int16_t topLim, int8_t inc
 		}
 		// found nothing
 		else{
-			return n_cols +1;
+			//cout << "not in range ";
+			return -2;
 		}
 	}
 }
 
 // private boi, colour from low to high 
-void Puzzle::push_to_colour(int16_t low, int16_t high, uint16_t line) {
+void Puzzle::push_to_colour(int16_t low, int16_t high, uint16_t line, bool isCol) {
+	cout << "col: " << line << endl;
 	for (int ind = low; ind <= high; ind++) {
 
 		// if the cell hasn't been coloured yet
 		uint16_t colind = n_cols*line+ind;
+		// adjust index accordingly if you're iterating thru columns
+		if (isCol) {
+			colind = n_cols*ind + line;
+		}
+		// if the cell hasn't been coloured yet
 		if (cells[colind]== -1) {
 			// colour it
 			cells[colind] = 0;
+			cout << colind << endl;
 			solved_indicator++;
 		}
 	}
@@ -119,47 +144,67 @@ void Puzzle::expand_and_limit(uint16_t total, uint16_t perpTotal, vector<vector<
 	vector<vector<range>> *black_runs, bool isCol){
 
 	// iterates thru all lines 
-	for (uint16_t line = 0; line < n_rows; line++){
+	for (uint16_t line = 0; line < total; line++){
 
 		// iterate thru all cells in the line
-		for (uint16_t celli = 0; celli < n_cols; celli++) {
-			//celli is the column number 0-n_col
-
+		for (uint16_t celli = 0; celli < perpTotal; celli++) {
+			// if doing it with rows celli is the column number 0-n_col
+			// if doing it with cols, celli is the row number
 			// find a black cell
-			if (cells[n_cols*line+celli] == 1) {
 
+			uint16_t cellInd;
+			// alter index accordingly depending if you're iterating thru columns or rows
+			if (isCol) {
+				cellInd = n_cols*celli +line;
+			}
+			else {
+				cellInd = n_cols*line+celli;
+			}
+
+			
+			if (cells[cellInd] == 0) {
+
+				
 				// find the smallest run that could be in celli
-				uint16_t runs_per_line = row_black_runs[line].size();
+				uint16_t runs_per_line = (*black_runs)[line].size();
 				vector <uint16_t> runs_in_range;
 
 				for (uint16_t run = 0; run <runs_per_line; run++){
 
-					range my_range = row_black_runs[line][run];
+					range my_range = (*black_runs)[line][run];
 					// if the run's range includes the cell
 					if (celli >= my_range.first && celli <= my_range.second) {
 						// push the length of the run
-						runs_in_range.push_back(row_restrictions[line][run]);
+						runs_in_range.push_back((*restrictions)[line][run]);
 					}
 				}
 				// find minimum length 
 				uint16_t minlen = *min_element(runs_in_range.begin(), runs_in_range.end());
 
+				//cout << "ind: " << cellInd << "smallest: " << minlen << endl;
+
 				// expand before?
 				// check to see if there is a white or a wall before the black
-				int16_t before = find_white_or_wall(celli-minlen+1, celli-1, -1);
+				//cout << "celli: " << celli << "|| minlen: " << minlen << endl;
+				int16_t before = find_white_or_wall(celli-minlen+1, celli-1, -1, perpTotal, line, isCol);
+
+				//cout << "before: " << before << endl;
 
 				// expand after?
 				// check to see if there is a white or a wall after the black
-				int16_t after = find_white_or_wall(celli+1, celli+minlen-1, 1);
+				int16_t after = find_white_or_wall(celli+1, celli+minlen-1, 1, perpTotal, line, isCol);
+
+				// cout << "after: " << after << endl;
+				// cout << endl;
 
 				// if there's a white or a wall, go colour
-				if (before != n_cols+1) {
-					push_to_colour(celli+1, before + minlen, line);
+				if (before != -2) {
+					push_to_colour(celli+1, before + minlen, line, isCol);
 				}
-				if (after != n_cols+1) {
-					push_to_colour(after-minlen, celli-1, line);
+				if (after != -2) {
+					push_to_colour(after-minlen, celli-1, line, isCol);
 				}
-				// try to find another black cell
+				//try to find another black cell
 			}
 
 			// if there are no black cells, rule does not apply
